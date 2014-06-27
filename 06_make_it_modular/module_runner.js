d448dab1709b7df0e8ab065535f292ab6dcde0e0
epeddireddy <epeddireddy@thetus.com>
2014-06-26 22:02:17 -0700

    learnyounode modifications

--- 06_make_it_modular/module_runner.js
+++ 06_make_it_modular/module_runner.js
@@ -3,7 +3,21 @@ var myModule = require('./mymodule'),
     fileExt = process.argv[3],
     callback = process.argv[4];
 
-//var callback = console.log;
-// var path = ../06_make_it_modular/
-
 myModule(directory, fileExt, callback);
+
+
+
+// Given solution:
+
+//var filterFn = require('./solution_filter.js')
+//var dir = process.argv[2]
+//var filterStr = process.argv[3]
+//
+//filterFn(dir, filterStr, function (err, list) {
+//    if (err)
+//        return console.error('There was an error:', err)
+//
+//    list.forEach(function (file) {
+//        console.log(file)
+//    })
+//})
--- 06_make_it_modular/mymodule.js
+++ 06_make_it_modular/mymodule.js
@@ -25,3 +25,24 @@ module.exports = function (directory, fileExt, callback) {
         callback(null, logIt());
     });
 };
+
+
+
+// Given solution:
+
+//var fs = require('fs')
+//var path = require('path')
+//
+//module.exports = function (dir, filterStr, callback) {
+//
+//    fs.readdir(dir, function (err, list) {
+//        if (err)
+//            return callback(err)
+//
+//        list = list.filter(function (file) {
+//            return path.extname(file) === '.' + filterStr
+//        })
+//
+//        callback(null, list)
+//    })
+//}
